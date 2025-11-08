28e1db970bb774a01ddad4eab326694257898564
Ashfaqazad <ashfaqahmedazad@gmail.com>
2025-11-08 21:07:05 +0500

    Update API calls to use Vercel proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    ILOVEPDF_PUBLIC_KEY: process.env.ILOVEPDF_PUBLIC_KEY,
-    ILOVEPDF_SECRET_KEY: process.env.ILOVEPDF_SECRET_KEY,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://72.60.78.58:4000/:path*", // Proxy to VPS backend
+      },
+    ];
   },
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   env: {
+//     ILOVEPDF_PUBLIC_KEY: process.env.ILOVEPDF_PUBLIC_KEY,
+//     ILOVEPDF_SECRET_KEY: process.env.ILOVEPDF_SECRET_KEY,
+//   },
+// };
+
+// export default nextConfig;
 
 
 
