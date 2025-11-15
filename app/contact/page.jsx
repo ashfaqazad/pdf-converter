ff6953b0814db306e0b74abe3aa2ba01cfdbef12
Ashfaqazad <ashfaqahmedazad@gmail.com>
2025-11-15 11:11:00 +0500

    Contact page updated

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -29,7 +29,7 @@ export default function Contact() {
               <label className="block text-gray-700 font-medium mb-1">Email</label>
               <input
                 type="email"
-                placeholder="you@example.com"
+                placeholder="@pdflinx.com"
                 className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
             </div>
